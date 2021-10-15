c6d27eb7e591dcb316ac57280d6bc505d2564f2b
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-10-15 16:07:10 +0000

    added temperature.cpp

---
+++ temperature.cpp
@@ -0,0 +1,43 @@
+// Copyright (c) 2021 Mikayla Barthelette All rights reserved
+//
+// Created by: Mikayla Barthelette
+// Created on: Oct 2021
+// This program converts from celsius to fahrenheit
+
+#include <iostream>
+#include <string>
+
+void CalculateFahrenheit() {
+    // calculate fahrenheit
+
+    std::string userInput;
+    float celsius;
+    float fahrenheit;
+
+    // input
+    std::cout << "Enter a temperature (°C): ";
+    std::cin >> userInput;
+
+    // process
+    try {
+        celsius = std::stoi(userInput);
+        fahrenheit = celsius * 1.8 + 32;
+        // would not do math properly with (9 / 5)
+        std::cout << celsius << "°C is equal to " << fahrenheit <<
+        "°F."  << std::endl;
+        std::cout << std::endl;
+    } catch (std::invalid_argument) {
+        std::cout << "Invalid input"  << std::endl;
+        std::cout << std::endl;
+    }
+
+    // output
+    std::cout << "Done." << std::endl;
+}
+
+main() {
+    // this function just calls other functions
+
+    // call functions
+    CalculateFahrenheit();
+}
